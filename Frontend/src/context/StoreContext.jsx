f23b099bb9af98d32ab99902a7a81dd04b7aeb25
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2024-12-21 23:24:15 +0530

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "https://cloud-kitchen-6.onrender.com/";
+    const url = "https://cloud-kitchen-6.onrender.com";
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
 
