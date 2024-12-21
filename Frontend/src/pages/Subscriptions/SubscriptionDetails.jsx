0b26d974e9840559efe326d6605929092c56cb01
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2024-12-21 23:26:18 +0530

    Update SubscriptionDetails.jsx

--- Frontend/src/pages/Subscriptions/SubscriptionDetails.jsx
+++ Frontend/src/pages/Subscriptions/SubscriptionDetails.jsx
@@ -8,7 +8,7 @@ const PlanDetails = () => {
     const [plan, setPlan] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const url = "http://localhost:4000"; // Backend API URL
+    const url = "https://cloud-kitchen-6.onrender.com"; // Backend API URL
 
     // State for user selection
     const [mealType, setMealType] = useState("breakfast");
