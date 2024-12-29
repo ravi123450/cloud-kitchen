62e294a9ed9dc96933147a4ebfc3972b7e03ddda
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2024-12-29 20:23:18 +0530

    Update AdminSubscriptions.jsx

--- admin/src/AdminSubscriptions.jsx
+++ admin/src/AdminSubscriptions.jsx
@@ -9,7 +9,7 @@ const AdminSubscriptions = () => {
     const [selectedUser, setSelectedUser] = useState(null); // State for selected user details
     const [showModal, setShowModal] = useState(false); // State to toggle modal visibility
 
-    const url = "https://cloud-kitchen-9.onrender.com/";
+    const url = "https://cloud-kitchen-9.onrender.com";
 
     useEffect(() => {
         const fetchSubscriptions = async () => {
